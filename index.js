c61f27fb227063d8bf0abb5991be27a67049646e
JER.8789 <je.rogers8789@gmail.com>
2021-03-31 19:33:10 -0500

    added if/else statements for new members

---
+++ index.js
@@ -0,0 +1,60 @@
+const fs = require('fs')
+const inquirer = require('inquirer')
+const jest = require('jest')
+const Engineer = require('./empClasses/engineer');
+const Manager = require('./empClasses/manager');
+const Intern = require('./empClasses/intern');
+
+function buildProfile(response) {
+    inquirer.prompt([{
+        name: 'ManagerName',
+        type: 'input',
+        message: 'Please Enter the Team Managers name.'
+    },{
+        name: 'employeeID',
+        type:'input',
+        message: 'Please enter your employee ID number'
+    },{
+        name: 'ManagerEmail',
+        type: 'input',
+        message: 'Please enter the managers email address.',
+    },{
+        name:'offNum',
+        type: 'input',
+        message: 'Please enter your office number.',
+    }])
+    .then(function(response) {
+     inquirer.prompt([{
+         name: 'addMember',
+         type: 'list',
+         message: 'Choose new team member role',
+         choices: ['engineer', 'intern']
+     }])
+     if (response.addMember === 'engineer') {
+         inquirer.prompt([{
+             name: 'name',
+             type: 'input',
+             message: 'please enter the engineers name',
+         },{
+             name:'email',
+             type:'input',
+             message:'Please enter engineer email address.'
+         },{
+             name:'github',
+             type: 'input',
+             message:'Please enter Github Username.'
+         }]);
+        } else if (response.addMember === 'intern') {
+            inquirer.prompt([{
+                name:'name',
+                type:'input',
+                message:'Please enter Intern name.'
+            },{
+                name:'email',
+                type:'input',
+                message:'Please enter intern email.'
+            },{}
+            }])
+        }
+    }
+}
