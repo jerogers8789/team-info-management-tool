49f3bb79437af2d40fbca0f1294e57b1c2bdc5ad
JER.8789 <je.rogers8789@gmail.com>
2021-04-07 17:45:03 -0500

    added classes and inquirer questions.

--- index.js
+++ index.js
@@ -12,11 +12,58 @@ function Employee(name, ID, email){
 }
 
 class Manager extends Employee {
-x
+ constructor(officeNum) {
+     this.officeNum = officeNum;
+ }
 }
-
-function buildProfile() {
-
+class Engineer extends Employee {
+    constructor(gitHub) {
+        this.gitHub = gitHub;
+    }
+}
+class Intern extends Employee {
+    constructor(school) {
+        this.school = school;
+    }
+}
+function buildProfile(response) {
+inquirer.prompt([{
+    name: 'name',
+    type: 'input',
+    message: 'Enter employee name'
+},{
+    name: 'ID',
+    type: 'input',
+    message: 'Enter employee ID'
+},{
+    name: 'email',
+    type: 'input',
+    message: 'Enter employee email'
+},{
+    name: 'role',
+    type: 'list',
+    message: 'Please select employee role',
+    choices: ['Manager', 'Engineer', 'Intern',]
+}]);
+ if (inquirer.prompt(role.response) === Manager) {  
+     inquirer.prompt([{
+         name: 'officeNum',
+         type: 'input',
+         message: 'Please Enter Manager office number.'
+     }]);
+ } else if (inquirer.prompt(role.response) === Engineer) {
+     inquirer.prompt([{
+         name: 'gitHub',
+         type: 'input',
+         message: 'Please Enter Engineer Github username.'
+     }]);
+ } else if (inquirer.prompt(role.response)=== Intern) {
+     inquirer.prompt([{
+         name: 'school',
+         type:'input',
+         message:'Please Enter the name of the school attended.'
+     }]);
+ }
 }
     
 buildProfile();
